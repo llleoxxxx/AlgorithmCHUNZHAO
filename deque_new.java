659b7497fc397674c219078e4dab49b784f2acbd
aaaa <68833368+llleoxxxx@users.noreply.github.com>
2021-01-24 15:58:24 +1030

    Create deque_new.java

---
+++ deque_new.java
@@ -0,0 +1,28 @@
+
+class Solution {
+    public void main(String []args) {
+
+          Deque<String> deque = new LinkedList<String>();
+
+          //根据文档，deque的push相当于在头部添加元素
+          deque.addFirst("a"); 
+          deque.addFirst("b");
+          deque.addFirst("c");
+
+
+          System.out.println(deque);
+          
+          //peek 用 peekFirst 替代
+          String str = deque.peekFirst(); 
+          System.out.println(str);
+          System.out.println(deque);
+          
+          
+          //remove 用 removeFirst 替代
+          while(deque.size() > 0){
+              System.out.println(deque.removeFirst()); 
+          }
+          System.out.println(deque);
+        }
+        
+}
