8a1a775e7f2c112a2817c123127104f185eb6f3a
aaaa <68833368+llleoxxxx@users.noreply.github.com>
2021-01-28 16:51:00 +1030

    Update levelOrder.java

--- Week_02/levelOrder.java
+++ Week_02/levelOrder.java
@@ -17,6 +17,50 @@ class Node {
 };
 */
 
+/*
+// Definition for a Node.
+class Node {
+    public int val;
+    public List<Node> children;
+
+    public Node() {}
+
+    public Node(int _val) {
+        val = _val;
+    }
+
+    public Node(int _val, List<Node> _children) {
+        val = _val;
+        children = _children;
+    }
+};
+*/
+
+class Solution {
+    public List<List<Integer>> levelOrder(Node root) {
+        
+        //用queue来存储节点，用list来存储数值
+        List<Integer> values = new ArrayList<>();
+        Queue<Node> queue = new LinkedList<>();
+        queue.add(root);
+
+        while (!queue.isEmpty()) {
+            Node nextNode = queue.remove();
+            values.add(nextNode.val);
+
+        for (Node child : nextNode.children) {
+        queue.add(child);
+        }
+        }
+
+}
+
+
+
+
+
+
+
 
 //尝试用递归写，失败了
 
