8352b81fa9f6cf8a279fb73d623acde621aea2c5
aaaa <68833368+llleoxxxx@users.noreply.github.com>
2021-01-28 14:48:55 +1030

    Create preorderTraversal.java

---
+++ Week_02/preorderTraversal.java
@@ -0,0 +1,46 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+
+
+class Solution {
+
+   //这里应该是先申明一个list
+   List<Integer> result_arr ;
+
+   public List<Integer> preorderTraversal(TreeNode root) {
+        
+        result_arr = new ArrayList<>() ;
+        order(root) ;
+        return result_arr;
+
+    }
+
+    public void order(TreeNode root){
+ 
+        if(root == null){
+        // return 这里也要加 ; 分号 
+            return ;
+        }
+        result_arr.add(root.val); 
+        
+        order(root.left); 
+        order(root.right); 
+
+        
+    }
+
+
+}
