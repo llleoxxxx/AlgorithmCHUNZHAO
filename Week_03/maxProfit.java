b29177eed9e95de4933271c3613df7067a9423ad
aaaa <68833368+llleoxxxx@users.noreply.github.com>
2021-02-04 14:41:00 +1030

    Create maxProfit.java

---
+++ Week_03/maxProfit.java
@@ -0,0 +1,20 @@
+
+class Solution {
+
+    public int maxProfit(int[] prices) {
+        int len = prices.length;
+        if (len < 2) {
+            return 0;
+        }
+
+        int res = 0;
+        for (int i = 1; i < len; i++) {
+            int diff = prices[i] - prices[i - 1];
+            if (diff > 0) {
+                res += diff;
+            }
+        }
+        return res;
+    }
+}
+
