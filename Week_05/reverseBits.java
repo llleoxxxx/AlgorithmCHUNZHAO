c51b234eb805eea9531c9e4830187c36898a9155
aaaa <68833368+llleoxxxx@users.noreply.github.com>
2021-02-27 13:47:55 +0800

    Update reverseBits.java

--- Week_05/reverseBits.java
+++ Week_05/reverseBits.java
@@ -1,12 +1,11 @@
 public class Solution {
-    // you need treat n as an unsigned value
     public int reverseBits(int n) {
-        int res=0;
-        for(int i=0;i<32;++i){
-            res=res<<1;
-            res=n&1|res;
-            n=n>>1;
+        int result = 0;
+        for (int i = 0; i < 32; i++) {
+            result <<= 1;
+            result += 1 & n;
+            n >>= 1;
         }
-        return res;
+        return result;
     }
 }
