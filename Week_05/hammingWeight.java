059d54dbcb00356dac206a74cd488fa714722f44
aaaa <68833368+llleoxxxx@users.noreply.github.com>
2021-02-27 12:53:34 +0800

    Update hammingWeight.java

--- Week_05/hammingWeight.java
+++ Week_05/hammingWeight.java
@@ -2,7 +2,7 @@ public int hammingWeight(int n) {
     int sum = 0;
     while (n != 0) {
         sum++;
-        n &= (n - 1); // 这里清零最低位的1
+        n &= (n - 1); // 这里清零最低位的1 ，具体原理可以看图解
     }
     return sum;
 }
