cd65909da03bfe1ecbb9facca7eb1393e947b84a
aaaa <68833368+llleoxxxx@users.noreply.github.com>
2021-02-27 13:05:25 +0800

    Update isPowerOfTwo.java

--- Week_05/isPowerOfTwo.java
+++ Week_05/isPowerOfTwo.java
@@ -2,7 +2,7 @@ class Solution {
   public boolean isPowerOfTwo(int n) {
     if (n == 0) return false;
     long x = (long) n;
-    return (x & (x - 1)) == 0;
+    return (x & (x - 1)) == 0;  //// 这里清零最低位的1 ，如果是2的幂，那么只存在一个1
   }
 }
 
